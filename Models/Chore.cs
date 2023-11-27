9de8119beff297e6898a9ea209252f932255c79e
MarcS2 <marcellosharrai@hotmail.com>
2023-11-27 15:06:36 -0700

    ChoreScore

---
+++ Models/Chore.cs
@@ -0,0 +1,11 @@
+
+
+namespace ChoreScore.Models
+{
+    public class Chore
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
